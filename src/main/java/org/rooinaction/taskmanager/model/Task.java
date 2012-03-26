b54ff2332b9ba6b3ba078fd7bbc9fffbe52763fc
Roo Git Add-On <s2-roo@vmware.com>
2012-03-26 19:02:33 -0400

    git setup

---
+++ src/main/java/org/rooinaction/taskmanager/model/Task.java
@@ -0,0 +1,21 @@
+package org.rooinaction.taskmanager.model;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Task {
+
+    @NotNull
+    @Size(max = 40)
+    private String description;
+
+    @Value("false")
+    private Boolean completed;
+}
---
+++ src/test/java/org/rooinaction/taskmanager/model/TaskIntegrationTest.java
@@ -0,0 +1,12 @@
+package org.rooinaction.taskmanager.model;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Task.class)
+public class TaskIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
